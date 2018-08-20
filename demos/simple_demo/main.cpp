e664f38db59b5ba0f253c2cd95c0f05b34365fef
Yuki Koyama <yuki@koyama.xyz>
2018-08-21 08:42:35 +0900

    Implemnet a minimum demo

--- demos/simple_demo/main.cpp
+++ demos/simple_demo/main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
 #include <bvh11/bvh11.hpp>
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2)
+    {
+        std::cerr << "Usage: simpe_demo [BVH_PATH]" << std::endl;
+        exit(1);
+    }
+    
+    bvh11::BvhObject bvh(argv[1]);
+
     return 0;
 }
