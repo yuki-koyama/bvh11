e3263cfe261c8d20fc2fcc104614c463d98ba2de
Yuki Koyama <yuki@koyama.xyz>
2018-09-09 14:28:56 +0900

    Change the behaviors of demo programs when no BVH is specified

--- demos/simple_demo/main.cpp
+++ demos/simple_demo/main.cpp
@@ -6,10 +6,12 @@ int main(int argc, char* argv[])
     if (argc < 2)
     {
         std::cerr << "Usage: simpe_demo [BVH_PATH]" << std::endl;
-        exit(1);
+        std::cout << "Loading a default BVH file..." << std::endl;
     }
     
-    bvh11::BvhObject bvh(argv[1]);
+    const std::string bvh_file_path = (argc >= 2) ? argv[1] : "131_03.bvh";
+
+    bvh11::BvhObject bvh(bvh_file_path);
     
     std::cout << "#Channels       : " << bvh.channels().size() << std::endl;
     std::cout << "#Frames         : " << bvh.frames()          << std::endl;
--- demos/visual_demo/main.cpp
+++ demos/visual_demo/main.cpp
@@ -7,12 +7,14 @@ int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cerr << "Usage: simpe_demo [BVH_PATH]" << std::endl;
-        exit(1);
+        std::cerr << "Usage: visual_demo [BVH_PATH]" << std::endl;
+        std::cout << "Loading a default BVH file..." << std::endl;
     }
+
+    const std::string bvh_file_path = (argc >= 2) ? argv[1] : "131_03.bvh";
     
     // Instantiate a BVH object
-    bvh11::BvhObject bvh(argv[1]);
+    bvh11::BvhObject bvh(bvh_file_path);
     
     // Instantiate a widget for visualizing BVH object
     QApplication app(argc, argv);
