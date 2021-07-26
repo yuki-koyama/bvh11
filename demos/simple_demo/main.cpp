21c28b4b76bcc828359b1365515d7efcc8f00f65
Yuki Koyama <yuki@koyama.xyz>
2021-07-26 19:12:25 +0900

    Update the demo code

--- demos/simple_demo/main.cpp
+++ demos/simple_demo/main.cpp
@@ -11,13 +11,21 @@ int main(int argc, char* argv[])
 
     const std::string bvh_file_path = (argc >= 2) ? argv[1] : "131_03.bvh";
 
+    // Import data from a BVH file
     bvh11::BvhObject bvh(bvh_file_path);
 
+    // Display basic info
     std::cout << "#Channels       : " << bvh.channels().size() << std::endl;
     std::cout << "#Frames         : " << bvh.frames() << std::endl;
     std::cout << "Frame time      : " << bvh.frame_time() << std::endl;
     std::cout << "Joint hierarchy : " << std::endl;
     bvh.PrintJointHierarchy();
+
+    // Edit the motion data (here, the number of frames is reduced into the half)
+    bvh.ResizeFrames(bvh.frames() / 2);
+ 
+    // Export data to a BVH file
+    bvh.WriteBvhFile("./out.bvh");
 
     return 0;
 }
